e7d22aa6b8fc00e5e20c031478ae4e480a0026ec
Ibrahim <ibrahimnas1@yahoo.com>
2025-02-18 14:20:48 +0000

    header ui

---
+++ apps/website/src/app/ui/header.tsx
@@ -0,0 +1,57 @@
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "@repo/ui/button";
+
+export default function Header() {
+    return (
+        <header>
+            <div>
+                <div className="flex max-w-6xl m-auto mt-3 px-6 py-1 place-items-center max-sm:px-3">
+                    <div className=" hidden grow sm:block">
+                        <Image 
+                        src={"/FarmVesta logo1.png"} 
+                        alt={"FarmVesta Logo"}
+                        width={150}
+                        height={760}
+                        className="" 
+                        />
+                    </div>
+
+                    <div className="grow sm:hidden">
+                        <Image 
+                        src={"/favicon.png"} 
+                        alt={"FarmVesta Logo"}
+                        width={50}
+                        height={760}
+                        className="" 
+                        />
+                    </div>
+
+                    <div >
+                        <ul className="flex space-x-7 text-sm items-center max-sm:space-x-4">
+                            <li>
+                              <Link href={'/'}>
+                                Home
+                              </Link>
+                            </li>
+                            <li>
+                              <Link href={'/'}>
+                                About us
+                              </Link>
+                            </li>
+                            <li>
+                              <Link href={'/'}>
+                                Blog
+                              </Link>
+                            </li>
+                            <li>
+                              <Button className="rounded-2xl bg-[#6da446] py-1 px-3 text-white hover:bg-[#83c746]" appName={"FarmVesta"}>Contact us</Button>
+                            </li>
+                            
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+}
