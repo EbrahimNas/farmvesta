70b6a25da468b9ac685b77e5e1e95dacc62d1da8
Ibrahim <ibrahimnas1@yahoo.com>
2025-02-23 04:24:17 +0000

    preview error fix

--- apps/website/src/app/blog/[slug]/page.tsx
+++ apps/website/src/app/blog/[slug]/page.tsx
@@ -2,10 +2,18 @@ import { notFound } from "next/navigation";
 import Header from "@/app/components/header";
 import Footer from "@/app/components/footer";
 import { placeholderBlogs } from "@/app/lib/placeholder-data";
+import { Blog } from "@/app/lib/definitions";
 import Image from "next/image";
 
-export default function BlogPost({ params }: { params: { slug: string } }) {
-  const blog = placeholderBlogs.find((post) => post.slug === params.slug);
+
+type BlogPostProps = {
+    params: {
+      slug: string;
+    };
+  };
+
+export default function BlogPost({ params }: BlogPostProps) {
+    const blog: Blog | undefined = placeholderBlogs.find((post) => post.slug === params.slug);
 
   if (!blog) return notFound(); // Show 404 if not found
 
@@ -25,7 +33,8 @@ export default function BlogPost({ params }: { params: { slug: string } }) {
                     alt={blog.title}
                     width={500}
                     height={100}
-                    className="rounded-2xl mb-8" />
+                    className="rounded-2xl mb-8" 
+                    />
               </div>
               <div className="rounded-2xl bg-[#ebebeb] dark:bg-[#363636] p-6">
                   <p>{blog.content}</p>
